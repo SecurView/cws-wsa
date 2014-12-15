0e52df35d6c4a97020cbd0ff70f30e0f262a8540
kgadekar <kgadekar@securview.com>
2014-12-15 16:35:17 +0530

    behavior added

--- src/main/java/com/cisco/policyconversiontool/dto/wsa/wsanormalized/WSAApplication.java
+++ src/main/java/com/cisco/policyconversiontool/dto/wsa/wsanormalized/WSAApplication.java
@@ -6,6 +6,7 @@ import java.io.Serializable;
 public class WSAApplication  implements Serializable{
 	private String id;
 	private String action;
+	private String behavior;
 	public String getId() {
 		return id;
 	}
@@ -17,6 +18,12 @@ public class WSAApplication  implements Serializable{
 	}
 	public void setAction(String action) {
 		this.action = action;
+	}
+	public String getBehavior() {
+		return behavior;
+	}
+	public void setBehavior(String behavior) {
+		this.behavior = behavior;
 	}
 
 	
