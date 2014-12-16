4891d55c1a00589581a84f8b0554cc803e2269a9
kgadekar <kgadekar@securview.com>
2014-12-16 15:20:28 +0530

    input stream changed to string

--- src/main/java/com/cisco/policyconversiontool/service/wsa/migrator/ApplianceXMLGenerator.java
+++ src/main/java/com/cisco/policyconversiontool/service/wsa/migrator/ApplianceXMLGenerator.java
@@ -1,11 +1,10 @@
 package com.cisco.policyconversiontool.service.wsa.migrator;
 
-import java.io.OutputStream;
 
 import com.cisco.policyconversiontool.dto.wsa.wsanormalized.WSAMigratedConfig;
 
 
 
 public interface ApplianceXMLGenerator {
-	public OutputStream generateXML(WSAMigratedConfig wsaMigratedConfig,Object objWSAInitialConfig) throws Exception;
+	public String generateXML(WSAMigratedConfig wsaMigratedConfig,Object objWSAInitialConfig) throws Exception;
 }
