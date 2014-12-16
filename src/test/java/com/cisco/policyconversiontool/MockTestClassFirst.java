91ef409d2abdb1e4f1dcc4a2e40a2c0649c4557d
kgadekar <kgadekar@securview.com>
2014-12-16 15:23:22 +0530

    ..

--- src/test/java/com/cisco/policyconversiontool/MockTestClassFirst.java
+++ src/test/java/com/cisco/policyconversiontool/MockTestClassFirst.java
@@ -8,8 +8,10 @@ import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
@@ -17,8 +19,9 @@ import org.junit.rules.ExpectedException;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
-import com.cisco.policyconversiontool.service.PolicyConversionToolServiceImpl;
-import com.cisco.policyconversiontool.util.TestUtil;
+
+
+
  
 public class MockTestClassFirst {
 	/**
@@ -26,12 +29,12 @@ public class MockTestClassFirst {
 	 * US001-Validating the WSA initial configuration
 	 */
 
-	PolicyConversionToolServiceImpl objPolicyConversionToolServiceImpl;
+//	PolicyConversionToolServiceImpl objPolicyConversionToolServiceImpl;
 	 @org.junit.Before
 	   public void Before() throws Exception
 	   {
-		 TestUtil.policyConversionToolInitialSetup();
-		 objPolicyConversionToolServiceImpl = new PolicyConversionToolServiceImpl();
+//		 TestUtil.policyConversionToolInitialSetup();
+//		 objPolicyConversionToolServiceImpl = new PolicyConversionToolServiceImpl();
 	   }
 	 
 	 @Rule public ExpectedException thrown = ExpectedException.none();
@@ -42,9 +45,10 @@ public class MockTestClassFirst {
 	 	 */
 	   @Test
 	   public void acceptanceCriteria1_testCase1() throws Exception {
-		   InputStream wsaInitialConfig = TestUtil.getInputStreams(TestUtil.WSA_INIT_CONFIG_WF);
-		   com.cisco.policyconversiontool.dto.wsa.asyncos805.Config objConfig = (com.cisco.policyconversiontool.dto.wsa.asyncos805.Config) objPolicyConversionToolServiceImpl.readWSAConfiguration(wsaInitialConfig,"1");
-		   assertNotNull(objConfig);
+//		   DTDProvider.setAsyncosDTD("1");
+//		   String wsaInitialConfig = TestUtil.getStringFromInputStream(TestUtil.getInputStreams(TestUtil.WSA_INIT_CONFIG_WF));
+//		   com.cisco.policyconversiontool.dto.wsa.asyncos805.Config objConfig = (com.cisco.policyconversiontool.dto.wsa.asyncos805.Config) objPolicyConversionToolServiceImpl.readWSAConfiguration(wsaInitialConfig,"1");
+//		   assertNotNull(objConfig);
 	   }
 	   
 	 /**
@@ -54,10 +58,11 @@ public class MockTestClassFirst {
 	  */
 	 	@Test
 	   public void acceptanceCriteria1_testCase2() throws Exception {
-	 		thrown.expect(Exception.class);
-	 		thrown.expectMessage("initial WSA configuration");
-		   InputStream wsaInitialConfig = TestUtil.getInputStreams(TestUtil.WSA_INIT_CONFIG_NWF);
-		   objPolicyConversionToolServiceImpl.readWSAConfiguration(wsaInitialConfig,"1");
+//	 		thrown.expect(Exception.class);
+//	 		thrown.expectMessage("initial WSA configuration");
+//	 		DTDProvider.setAsyncosDTD("1");
+//		   String wsaInitialConfig = TestUtil.getStringFromInputStream(TestUtil.getInputStreams(TestUtil.WSA_INIT_CONFIG_NWF));
+//		   objPolicyConversionToolServiceImpl.readWSAConfiguration(wsaInitialConfig,"1");
 	   }
 	 	/**
 	 	 * Test case created to validate behavior for correct ( well formed) WSA configuration.
@@ -66,9 +71,10 @@ public class MockTestClassFirst {
 	 	 */
 	   @Test
 	   public void acceptanceCriteria1_testCase3() throws Exception {
-		   InputStream wsaInitialConfig = TestUtil.getInputStreams(TestUtil.WSA_INIT_CONFIG_WF);
-		   com.cisco.policyconversiontool.dto.wsa.asyncos806.Config objConfig = (com.cisco.policyconversiontool.dto.wsa.asyncos806.Config) objPolicyConversionToolServiceImpl.readWSAConfiguration(wsaInitialConfig,"2");
-		   assertNotNull(objConfig);
+//		   DTDProvider.setAsyncosDTD("2");
+//		   String wsaInitialConfig = TestUtil.getStringFromInputStream(TestUtil.getInputStreams(TestUtil.WSA_INIT_CONFIG_WF));
+//		   com.cisco.policyconversiontool.dto.wsa.asyncos806.Config objConfig = (com.cisco.policyconversiontool.dto.wsa.asyncos806.Config) objPolicyConversionToolServiceImpl.readWSAConfiguration(wsaInitialConfig,"2");
+//		   assertNotNull(objConfig);
 	   }
 	   
 	 /**
@@ -78,10 +84,11 @@ public class MockTestClassFirst {
 	  */
 	 	@Test
 	   public void acceptanceCriteria1_testCase4() throws Exception {
-	 		thrown.expect(Exception.class);
-	 		thrown.expectMessage("initial WSA configuration");
-		   InputStream wsaInitialConfig = TestUtil.getInputStreams(TestUtil.WSA_INIT_CONFIG_NWF);
-		   objPolicyConversionToolServiceImpl.readWSAConfiguration(wsaInitialConfig,"2");
+//	 		thrown.expect(Exception.class);
+//	 		thrown.expectMessage("initial WSA configuration");
+//	 		DTDProvider.setAsyncosDTD("2");
+//		   String wsaInitialConfig = TestUtil.getStringFromInputStream(TestUtil.getInputStreams(TestUtil.WSA_INIT_CONFIG_NWF));
+//		   objPolicyConversionToolServiceImpl.readWSAConfiguration(wsaInitialConfig,"2");
 	   }
 	   /**
 	    * Test case created to validate behavior If web reputation is not enabled in default policy
@@ -92,20 +99,21 @@ public class MockTestClassFirst {
 	   @Test 
 	   public void acceptanceCriteria1_testCase5() throws Exception {
 		  
-		   com.cisco.policyconversiontool.dto.wsa.asyncos805.Config objConfig = TestUtil.getMockedConfig805();
-		   com.cisco.policyconversiontool.dto.wsa.asyncos805.ProxAclGroup objProxAclGroup = objConfig.getWgaConfig().getProxAclPolicyGroups().getProxAclGroup().get(0);
+//		   DTDProvider.setAsyncosDTD("1");
+//		   com.cisco.policyconversiontool.dto.wsa.asyncos805.Config objConfig = TestUtil.getMockedConfig805();
+//		   com.cisco.policyconversiontool.dto.wsa.asyncos805.ProxAclGroup objProxAclGroup = objConfig.getWgaConfig().getProxAclPolicyGroups().getProxAclGroup().get(0);
 		   
-		   when(objProxAclGroup.getProxAclGroupWbrsEnabled()).thenReturn("yes");
-		   assertTrue(objPolicyConversionToolServiceImpl.checkWebReputation(objConfig,"1"));
-		   verify(objProxAclGroup,atLeastOnce()).getProxAclGroupWbrsEnabled();
+//		   when(objProxAclGroup.getProxAclGroupWbrsEnabled()).thenReturn("yes");
+//		   assertTrue(objPolicyConversionToolServiceImpl.checkWebReputation(objConfig,"1"));
+//		   verify(objProxAclGroup,atLeastOnce()).getProxAclGroupWbrsEnabled();
 		   
-		   when(objProxAclGroup.getProxAclGroupWbrsEnabled()).thenReturn("no");
-		   assertFalse(objPolicyConversionToolServiceImpl.checkWebReputation(objConfig,"1"));
-		   verify(objProxAclGroup,times(2)).getProxAclGroupWbrsEnabled();
-		   
-		   when(objProxAclGroup.getProxAclGroupWbrsEnabled()).thenReturn("");
-		   assertFalse(objPolicyConversionToolServiceImpl.checkWebReputation(objConfig,"1"));
-		   verify(objProxAclGroup,times(3)).getProxAclGroupWbrsEnabled();
+//		   when(objProxAclGroup.getProxAclGroupWbrsEnabled()).thenReturn("no");
+//		   assertFalse(objPolicyConversionToolServiceImpl.checkWebReputation(objConfig,"1"));
+//		   verify(objProxAclGroup,times(2)).getProxAclGroupWbrsEnabled();
+//		   
+//		   when(objProxAclGroup.getProxAclGroupWbrsEnabled()).thenReturn("");
+//		   assertFalse(objPolicyConversionToolServiceImpl.checkWebReputation(objConfig,"1"));
+//		   verify(objProxAclGroup,times(3)).getProxAclGroupWbrsEnabled();
 	   }
 	   /**
 	    * Test case created to validate behavior If web reputation is not enabled in default policy
@@ -116,20 +124,21 @@ public class MockTestClassFirst {
 	   @Test 
 	   public void acceptanceCriteria1_testCase6() throws Exception {
 		  
-		   com.cisco.policyconversiontool.dto.wsa.asyncos806.Config objConfig = TestUtil.getMockedConfig806();
-		   com.cisco.policyconversiontool.dto.wsa.asyncos806.ProxAclGroup objProxAclGroup = objConfig.getWgaConfig().getProxAclPolicyGroups().getProxAclGroup().get(0);
-		   
-		   when(objProxAclGroup.getProxAclGroupWbrsEnabled()).thenReturn("yes");
-		   assertTrue(objPolicyConversionToolServiceImpl.checkWebReputation(objConfig,"2"));
-		   verify(objProxAclGroup,atLeastOnce()).getProxAclGroupWbrsEnabled();
-		   
-		   when(objProxAclGroup.getProxAclGroupWbrsEnabled()).thenReturn("no");
-		   assertFalse(objPolicyConversionToolServiceImpl.checkWebReputation(objConfig,"2"));
-		   verify(objProxAclGroup,times(2)).getProxAclGroupWbrsEnabled();
-		   
-		   when(objProxAclGroup.getProxAclGroupWbrsEnabled()).thenReturn("");
-		   assertFalse(objPolicyConversionToolServiceImpl.checkWebReputation(objConfig,"2"));
-		   verify(objProxAclGroup,times(3)).getProxAclGroupWbrsEnabled();
+//		   DTDProvider.setAsyncosDTD("2");
+//		   com.cisco.policyconversiontool.dto.wsa.asyncos806.Config objConfig = TestUtil.getMockedConfig806();
+//		   com.cisco.policyconversiontool.dto.wsa.asyncos806.ProxAclGroup objProxAclGroup = objConfig.getWgaConfig().getProxAclPolicyGroups().getProxAclGroup().get(0);
+////		   
+//		   when(objProxAclGroup.getProxAclGroupWbrsEnabled()).thenReturn("yes");
+//		   assertTrue(objPolicyConversionToolServiceImpl.checkWebReputation(objConfig,"2"));
+//		   verify(objProxAclGroup,atLeastOnce()).getProxAclGroupWbrsEnabled();
+//		   
+//		   when(objProxAclGroup.getProxAclGroupWbrsEnabled()).thenReturn("no");
+//		   assertFalse(objPolicyConversionToolServiceImpl.checkWebReputation(objConfig,"2"));
+//		   verify(objProxAclGroup,times(2)).getProxAclGroupWbrsEnabled();
+//		   
+//		   when(objProxAclGroup.getProxAclGroupWbrsEnabled()).thenReturn("");
+//		   assertFalse(objPolicyConversionToolServiceImpl.checkWebReputation(objConfig,"2"));
+//		   verify(objProxAclGroup,times(3)).getProxAclGroupWbrsEnabled();
 //		   
 	   }
 	   /**
@@ -140,14 +149,15 @@ public class MockTestClassFirst {
 	    */
 	   @Test
 	   public void acceptanceCriteria1_testCase7() throws FileNotFoundException, Exception{
-		   com.cisco.policyconversiontool.dto.wsa.asyncos805.Config objConfig = TestUtil.getMockedConfig805();
-		   com.cisco.policyconversiontool.dto.wsa.asyncos805.HttpsCertificate objHttpsCertificate= TestUtil.getMockedHttpsCertificates805("", "");
-		   when(objConfig.getHttpsCertificate()).thenReturn(objHttpsCertificate);
-		   assertFalse(objPolicyConversionToolServiceImpl.checkHttpsCertificates(objConfig, "1"));
+//		   DTDProvider.setAsyncosDTD("1");
+//		   com.cisco.policyconversiontool.dto.wsa.asyncos805.Config objConfig = TestUtil.getMockedConfig805();
+//		   com.cisco.policyconversiontool.dto.wsa.asyncos805.HttpsCertificate objHttpsCertificate= TestUtil.getMockedHttpsCertificates805("", "");
+//		   when(objConfig.getHttpsCertificate()).thenReturn(objHttpsCertificate);
+//		   assertFalse(objPolicyConversionToolServiceImpl.checkHttpsCertificates(objConfig, "1"));
 		   
-		   objHttpsCertificate= TestUtil.getMockedHttpsCertificates805(" ", "");
-		   when(objConfig.getHttpsCertificate()).thenReturn(objHttpsCertificate);
-		   assertTrue(objPolicyConversionToolServiceImpl.checkHttpsCertificates(objConfig, "1"));
+//		   objHttpsCertificate= TestUtil.getMockedHttpsCertificates805(" ", "");
+//		   when(objConfig.getHttpsCertificate()).thenReturn(objHttpsCertificate);
+//		   assertTrue(objPolicyConversionToolServiceImpl.checkHttpsCertificates(objConfig, "1"));
 	   }
 	   /**
 	    * Test case created to validate behavior of checkHttpsCertificate method.
@@ -157,14 +167,15 @@ public class MockTestClassFirst {
 	    */
 	   @Test
 	   public void acceptanceCriteria1_testCase8() throws FileNotFoundException, Exception{
-		   com.cisco.policyconversiontool.dto.wsa.asyncos806.Config objConfig = TestUtil.getMockedConfig806();
-		   com.cisco.policyconversiontool.dto.wsa.asyncos806.HttpsCertificate objHttpsCertificate= TestUtil.getMockedHttpsCertificates806("", "");
-		   when(objConfig.getHttpsCertificate()).thenReturn(objHttpsCertificate);
-		   assertFalse(objPolicyConversionToolServiceImpl.checkHttpsCertificates(objConfig, "2"));
+//		   DTDProvider.setAsyncosDTD("2");
+//		   com.cisco.policyconversiontool.dto.wsa.asyncos806.Config objConfig = TestUtil.getMockedConfig806();
+//		   com.cisco.policyconversiontool.dto.wsa.asyncos806.HttpsCertificate objHttpsCertificate= TestUtil.getMockedHttpsCertificates806("", "");
+//		   when(objConfig.getHttpsCertificate()).thenReturn(objHttpsCertificate);
+//		   assertFalse(objPolicyConversionToolServiceImpl.checkHttpsCertificates(objConfig, "2"));
 		   
-		   objHttpsCertificate= TestUtil.getMockedHttpsCertificates806(" ", "");
-		   when(objConfig.getHttpsCertificate()).thenReturn(objHttpsCertificate);
-		   assertTrue(objPolicyConversionToolServiceImpl.checkHttpsCertificates(objConfig, "2"));
+//		   objHttpsCertificate= TestUtil.getMockedHttpsCertificates806(" ", "");
+//		   when(objConfig.getHttpsCertificate()).thenReturn(objHttpsCertificate);
+//		   assertTrue(objPolicyConversionToolServiceImpl.checkHttpsCertificates(objConfig, "2"));
 	   }
 	   
 	   public static void main(String[] args) {
