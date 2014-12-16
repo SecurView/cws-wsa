050bfa6485a5620748a2a40539cf585d689a069f
kgadekar <kgadekar@securview.com>
2014-12-16 15:14:54 +0530

    ..

--- src/main/java/com/cisco/policyconversiontool/ConsoleManager.java
+++ src/main/java/com/cisco/policyconversiontool/ConsoleManager.java
@@ -17,131 +17,132 @@ import com.cisco.policyconversiontool.dto.Appliance;
 import com.cisco.policyconversiontool.dto.Software;
 import com.cisco.policyconversiontool.dto.Vendor;
 import com.cisco.policyconversiontool.dto.WSAMigrationPageInfo;
-import com.cisco.policyconversiontool.dto.WSAMigrationParameters;
 public class ConsoleManager {
 	
 	private Scanner scanner = new Scanner(System.in);
 	
-	public void acceptParameter(WSAMigrationPageInfo pageInfo,WSAMigrationParameters wsaMigrationParameters) throws ParseException, IOException {
+	public void acceptParameter(WSAMigrationPageInfo pageInfo) throws ParseException, IOException {
 		
-		// Source Vendor List
-		printAcceptMessage("Accepting Source Information:\n\n");
-		for(Vendor Vendor:pageInfo.getSourceVendorList()){
-			printAcceptMessage(""+Vendor.getId()+". "+Vendor.getName()+"\n");
-		}
-		printAcceptMessage("\nSelect the Source vendor: ");
-		String sourceVendorId = scanner.next();
-		wsaMigrationParameters.setSourceVendor(sourceVendorId);
-		printAcceptMessage("\n");
-		
-		// Source Appliance List
-		for(Appliance appliance:pageInfo.getApplianceList(sourceVendorId)){
-			printAcceptMessage(appliance.getId()+".	"+appliance.getName()+"\n");
-		}
-		printAcceptMessage("\nSelect appliance: ");
-		wsaMigrationParameters.setSourceAppliance(scanner.next());
-		
-		// Source Software List
-		for(Software software:pageInfo.getSoftwareList(sourceVendorId)){
-			printAcceptMessage(""+software.getId()+". "+software.getName()+"\n");
-		}
-		printAcceptMessage("\nSelect Software: ");
-		wsaMigrationParameters.setSourceSoftware(scanner.next());
-		printAcceptMessage("\n");
-		// SourceFilePath 
-		printAcceptMessage("\nSelect the Source archieved file Path: ");
-		wsaMigrationParameters.setSourceArchievedFile(generate(scanner.next()));
-		printAcceptMessage("\n");
-//		printAcceptMessage("\nDo you want to change the Source Vendor now [y/n]: ");
-//		acceptSourceVendor(wsaMigrationParameters, pageInfo);
-		
-		
-
-		// Target Vendor List
-		printAcceptMessage("Accepting Target Information:\n\n");
-		for(Vendor Vendor:pageInfo.getTargetVendorList()){
-			printAcceptMessage(""+Vendor.getId()+".	"+Vendor.getName()+"\n");
-		}
-		printAcceptMessage("\nSelect Vendor: ");
-		String targetVendorId = scanner.next();
-		wsaMigrationParameters.setTargetVendor(targetVendorId);
-		printAcceptMessage("\n");
-		
-						
-		// Target Appliance List
-		for(Appliance appliance:pageInfo.getApplianceList(targetVendorId)){
-			printAcceptMessage(""+appliance.getId()+".	"+appliance.getName()+"\n");
-		}
-		printAcceptMessage("\nSelect Appliance: ");
-		wsaMigrationParameters.setTargetAppliance(scanner.next());
-		printAcceptMessage("\n");
-				
-		// Target Software List
-		for(Software software:pageInfo.getSoftwareList(targetVendorId)){
-			printAcceptMessage(""+software.getId()+". "+software.getName()+"\n");
-		}
-		printAcceptMessage("\nSelect Software: ");
-		wsaMigrationParameters.setTargetSoftware(scanner.next());
-		// TargetFilePath 
-		printAcceptMessage("\nSelect the Initial Target XML File Path: ");
-		wsaMigrationParameters.setTargetXMLFile(new FileInputStream(new File(scanner.next())));
-//		printAcceptMessage("\nDo you want to change the Target Vendor now [y/n]: ");
-//		acceptTargetVendor(wsaMigrationParameters,pageInfo);
-		
-		
-//		return wsaMigrationParameters;
+//		// Source Vendor List
+//		printAcceptMessage("Accepting Source Information:\n\n");
+//		for(Vendor Vendor:pageInfo.getSourceVendorList()){
+//			printAcceptMessage(""+Vendor.getId()+". "+Vendor.getName()+"\n");
+//		}
+//		printAcceptMessage("\nSelect the Source vendor: ");
+//		String sourceVendorId = scanner.next();
+//		wsaMigrationParameters.setSourceVendor(sourceVendorId);
+//		printAcceptMessage("\n");
+//		
+//		// Source Appliance List
+//		for(Appliance appliance:pageInfo.getApplianceList(sourceVendorId)){
+//			printAcceptMessage(appliance.getId()+".	"+appliance.getName()+"\n");
+//		}
+//		printAcceptMessage("\nSelect appliance: ");
+//		wsaMigrationParameters.setSourceAppliance(scanner.next());
+//		
+//		// Source Software List
+//		for(Software software:pageInfo.getSoftwareList(sourceVendorId)){
+//			printAcceptMessage(""+software.getId()+". "+software.getName()+"\n");
+//		}
+//		printAcceptMessage("\nSelect Software: ");
+//		wsaMigrationParameters.setSourceSoftware(scanner.next());
+//		printAcceptMessage("\n");
+//		// SourceFilePath 
+//		printAcceptMessage("\nSelect the Source archieved file Path: ");
+//		wsaMigrationParameters.setSourceArchievedFile(generate(scanner.next()));
+//		printAcceptMessage("\n");
+////		printAcceptMessage("\nDo you want to change the Source Vendor now [y/n]: ");
+////		acceptSourceVendor(wsaMigrationParameters, pageInfo);
+//		
+//		
+//
+//		// Target Vendor List
+//		printAcceptMessage("Accepting Target Information:\n\n");
+//		for(Vendor Vendor:pageInfo.getTargetVendorList()){
+//			printAcceptMessage(""+Vendor.getId()+".	"+Vendor.getName()+"\n");
+//		}
+//		printAcceptMessage("\nSelect Vendor: ");
+//		String targetVendorId = scanner.next();
+//		wsaMigrationParameters.setTargetVendor(targetVendorId);
+//		printAcceptMessage("\n");
+//		
+//						
+//		// Target Appliance List
+//		for(Appliance appliance:pageInfo.getApplianceList(targetVendorId)){
+//			printAcceptMessage(""+appliance.getId()+".	"+appliance.getName()+"\n");
+//		}
+//		printAcceptMessage("\nSelect Appliance: ");
+//		wsaMigrationParameters.setTargetAppliance(scanner.next());
+//		printAcceptMessage("\n");
+//				
+//		// Target Software List
+//		for(Software software:pageInfo.getSoftwareList(targetVendorId)){
+//			printAcceptMessage(""+software.getId()+". "+software.getName()+"\n");
+//		}
+//		printAcceptMessage("\nSelect Software: ");
+//		wsaMigrationParameters.setTargetSoftware(scanner.next());
+//		// TargetFilePath 
+//		printAcceptMessage("\nSelect the Initial Target XML File Path: ");
+//		wsaMigrationParameters.setTargetXMLFile(new FileInputStream(new File(scanner.next())));
+////		printAcceptMessage("\nDo you want to change the Target Vendor now [y/n]: ");
+////		acceptTargetVendor(wsaMigrationParameters,pageInfo);
+//		
+//		
+////		return wsaMigrationParameters;
 	}
-	public boolean acceptSourceVendor(WSAMigrationParameters wsaMigrationParameters,WSAMigrationPageInfo pageInfo) throws IOException{
-		if (scanner.next().trim().equalsIgnoreCase("y")){
-			for(Vendor Vendor:pageInfo.getSourceVendorList()){
-				printAcceptMessage(" \nVendorId- "+Vendor.getId()+"	Vendor Name- "+Vendor.getName()+"	Vendor Description- "+Vendor.getDesc());
-			}
-			printAcceptMessage("\nProvide Source Vendor Id: ");
-			String strVendorSourceId=scanner.next();
-			wsaMigrationParameters.setSourceVendor(strVendorSourceId);
-			for(Appliance appliance:pageInfo.getApplianceList(strVendorSourceId)){
-				printAcceptMessage("\n ApplianceId- "+appliance.getId()+"	Vendor Name- "+appliance.getName()+"	Vendor Description- "+appliance.getDesc());
-			}
-			printAcceptMessage("\nProvide Source Appliance Id: ");
-			wsaMigrationParameters.setSourceAppliance(scanner.next());
-			for(Software software:pageInfo.getSoftwareList(strVendorSourceId)){
-				printAcceptMessage(" \nSoftwareId- "+software.getId()+"	Software Name- "+software.getName()+"	Software Description- "+software.getDesc());
-			}
-			printAcceptMessage("\nProvide Source Software Id: ");
-			wsaMigrationParameters.setSourceSoftware(scanner.next());
-			printAcceptMessage("\nSelect the wsa migration Source Zip File Path:\n");
-			wsaMigrationParameters.setSourceArchievedFile(generate(scanner.next()));
-			return true;
-		}else{
-			return false;
-		}
+	public boolean acceptSourceVendor(WSAMigrationPageInfo pageInfo) throws IOException{
+		return false;
+//		if (scanner.next().trim().equalsIgnoreCase("y")){
+//			for(Vendor Vendor:pageInfo.getSourceVendorList()){
+//				printAcceptMessage(" \nVendorId- "+Vendor.getId()+"	Vendor Name- "+Vendor.getName()+"	Vendor Description- "+Vendor.getDesc());
+//			}
+//			printAcceptMessage("\nProvide Source Vendor Id: ");
+//			String strVendorSourceId=scanner.next();
+//			wsaMigrationParameters.setSourceVendor(strVendorSourceId);
+//			for(Appliance appliance:pageInfo.getApplianceList(strVendorSourceId)){
+//				printAcceptMessage("\n ApplianceId- "+appliance.getId()+"	Vendor Name- "+appliance.getName()+"	Vendor Description- "+appliance.getDesc());
+//			}
+//			printAcceptMessage("\nProvide Source Appliance Id: ");
+//			wsaMigrationParameters.setSourceAppliance(scanner.next());
+//			for(Software software:pageInfo.getSoftwareList(strVendorSourceId)){
+//				printAcceptMessage(" \nSoftwareId- "+software.getId()+"	Software Name- "+software.getName()+"	Software Description- "+software.getDesc());
+//			}
+//			printAcceptMessage("\nProvide Source Software Id: ");
+//			wsaMigrationParameters.setSourceSoftware(scanner.next());
+//			printAcceptMessage("\nSelect the wsa migration Source Zip File Path:\n");
+//			wsaMigrationParameters.setSourceArchievedFile(generate(scanner.next()));
+//			return true;
+//		}else{
+//			return false;
+//		}
 	}
 	
-	public boolean acceptTargetVendor(WSAMigrationParameters wsaMigrationParameters,WSAMigrationPageInfo pageInfo) throws IOException{
-		if (scanner.next().trim().equalsIgnoreCase("y")){
-			for(Vendor Vendor:pageInfo.getTargetVendorList()){
-				printAcceptMessage("\n VendorId- "+Vendor.getId()+"	Vendor Name- "+Vendor.getName()+"	Vendor Description- "+Vendor.getDesc());
-				}
-			printAcceptMessage("\nProvide Target Vendor Id: ");
-			String strTargetSourceId=scanner.next(); 
-			wsaMigrationParameters.setTargetVendor(strTargetSourceId);
-			for(Appliance appliance:pageInfo.getApplianceList(strTargetSourceId)){
-				printAcceptMessage("\n ApplianceId- "+appliance.getId()+"	Vendor Name- "+appliance.getName()+"	Vendor Description- "+appliance.getDesc());
-			}
-			printAcceptMessage("\nProvide Target Appliance Id: ");
-			wsaMigrationParameters.setTargetAppliance(scanner.next());
-			for(Software software:pageInfo.getSoftwareList(strTargetSourceId)){
-				printAcceptMessage(" \nSoftwareId- "+software.getId()+"	Software Name- "+software.getName()+"	Software Description- "+software.getDesc());
-			}
-			printAcceptMessage("\nProvide Target Software Id: ");
-			wsaMigrationParameters.setTargetSoftware(scanner.next());
-			// TargetFilePath 
-			printAcceptMessage("\nSelect the wsa migration Target XML File Path:\n");
-			wsaMigrationParameters.setTargetXMLFile(new FileInputStream(new File(scanner.next())));
-			return true;
-		}else{
-			return false;
-		}
+	public boolean acceptTargetVendor(WSAMigrationPageInfo pageInfo) throws IOException{
+		return false;
+//		if (scanner.next().trim().equalsIgnoreCase("y")){
+//			for(Vendor Vendor:pageInfo.getTargetVendorList()){
+//				printAcceptMessage("\n VendorId- "+Vendor.getId()+"	Vendor Name- "+Vendor.getName()+"	Vendor Description- "+Vendor.getDesc());
+//				}
+//			printAcceptMessage("\nProvide Target Vendor Id: ");
+//			String strTargetSourceId=scanner.next(); 
+//			wsaMigrationParameters.setTargetVendor(strTargetSourceId);
+//			for(Appliance appliance:pageInfo.getApplianceList(strTargetSourceId)){
+//				printAcceptMessage("\n ApplianceId- "+appliance.getId()+"	Vendor Name- "+appliance.getName()+"	Vendor Description- "+appliance.getDesc());
+//			}
+//			printAcceptMessage("\nProvide Target Appliance Id: ");
+//			wsaMigrationParameters.setTargetAppliance(scanner.next());
+//			for(Software software:pageInfo.getSoftwareList(strTargetSourceId)){
+//				printAcceptMessage(" \nSoftwareId- "+software.getId()+"	Software Name- "+software.getName()+"	Software Description- "+software.getDesc());
+//			}
+//			printAcceptMessage("\nProvide Target Software Id: ");
+//			wsaMigrationParameters.setTargetSoftware(scanner.next());
+//			// TargetFilePath 
+//			printAcceptMessage("\nSelect the wsa migration Target XML File Path:\n");
+//			wsaMigrationParameters.setTargetXMLFile(new FileInputStream(new File(scanner.next())));
+//			return true;
+//		}else{
+//			return false;
+//		}
 	}
 	
 	public static void printConsoleMessage(String message){
