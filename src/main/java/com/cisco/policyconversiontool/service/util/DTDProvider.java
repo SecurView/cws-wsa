3fdef5fb5208c48142a4014956b19330a46169e8
kgadekar <kgadekar@securview.com>
2014-12-16 17:29:00 +0530

    kept only one function to get dtd

--- src/main/java/com/cisco/policyconversiontool/service/util/DTDProvider.java
+++ src/main/java/com/cisco/policyconversiontool/service/util/DTDProvider.java
@@ -4,16 +4,12 @@ import java.io.File;
 
 public class DTDProvider {
 
-	private static File asyncosDTD;
-	public static void setAsyncosDTD(String software) {
+	public static File getAsyncosDTD(String software) {
 		if(software.equals(Constants.TARGET_SOWFWARE_WSA_ASYNCOS805))
-			asyncosDTD = new File(Constants.WSA_CONFIG_DTD_805_PATH);
+			return new File(Constants.WSA_CONFIG_DTD_805_PATH);
 		else if(software.equals(Constants.TARGET_SOWFWARE_WSA_ASYNCOS806))
-			asyncosDTD = new File(Constants.WSA_CONFIG_DTD_806_PATH);
+			return new File(Constants.WSA_CONFIG_DTD_806_PATH);
+		return null;
 	}
-	public static File getAsyncosDTD() {
-		return asyncosDTD;
-	}
-
 		
 }
