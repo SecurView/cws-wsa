f8e05ad9c02f59e1f40555d3cc6c705e75d9ad64
kgadekar <kgadekar@securview.com>
2014-12-16 15:14:27 +0530

    PolicyConversionParameters class added

--- src/main/java/com/cisco/policyconversiontool/AppController.java
+++ src/main/java/com/cisco/policyconversiontool/AppController.java
@@ -11,9 +11,10 @@ import org.apache.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.cisco.policyconversiontool.dto.WSAMigrationParameters;
+import com.cisco.policyconversiontool.dto.PolicyConversionParameters;
 import com.cisco.policyconversiontool.service.PolicyConversionToolService;
 import com.cisco.policyconversiontool.service.exception.PolicyConversionToolException;
+import com.cisco.policyconversiontool.service.util.CommonUtility;
 import com.cisco.policyconversiontool.service.util.DTDProvider;
 
 public class AppController {
@@ -22,39 +23,31 @@ public class AppController {
 
 	public void start(String args[]) throws PolicyConversionToolException, ParseException, IOException{
 		logger.info("***********************************");
-		logger.info("Started Log Analysing Tool");
+		logger.info("Started Policy Conversion Tool");
 		logger.info("***********************************");
-
-		DTDProvider.setAsyncos805DTD(new File("src//main//resource//config//wsaDTD805.dtd"));
-        DTDProvider.setAsyncos806DTD(new File("src//main//resource//config//wsaDTD806.dtd"));
-		
+/*Need to remove*/
+/**/		
 		ApplicationContext applicationContext=new ClassPathXmlApplicationContext("applicationContext.xml");
 		PolicyConversionToolService policyConversionToolService=(PolicyConversionToolService)applicationContext.getBean("PolicyConversionToolServiceImpl");
-		WSAMigrationParameters wsaMigrationParameters = new WSAMigrationParameters();
+		PolicyConversionParameters objPolicyConversionParameters = new PolicyConversionParameters();
 		ConsoleManager consoleManager = new ConsoleManager();
-		
-		ConsoleManager.printConsoleMessage("*****************************************");
-		ConsoleManager.printConsoleMessage("Welcome to CiscoProxyLogAnalyzerTool v1.0");
-		ConsoleManager.printConsoleMessage("*****************************************");
 //		consoleManager.acceptParameter(policyConversionToolService.getWSAMigrationPageInfo(), wsaMigrationParameters);
-		wsaMigrationParameters.setSourceAppliance("4");
-		wsaMigrationParameters.setSourceArchievedFile(new FileInputStream("src/test/resource/fixtures/cws/SecurView_6_CiscoFixes.json"));
-		wsaMigrationParameters.setSourceSoftware("7");
-		wsaMigrationParameters.setSourceVendor("4");
-		wsaMigrationParameters.setTargetVendor("1");
-		wsaMigrationParameters.setTargetAppliance("1");
-		wsaMigrationParameters.setTargetSoftware("1");
-		wsaMigrationParameters.setTargetXMLFile(new FileInputStream("src/test/resource/fixtures/wsa/AllSettings.xml"));
+		objPolicyConversionParameters.setSourceConfiguration(CommonUtility.getStringFromInputStream(new FileInputStream("src/test/resource/fixtures/cws/SecurView_6_CiscoFixes.json")));
+		objPolicyConversionParameters.setTargetAppliance("1");
+		objPolicyConversionParameters.setTargetSoftware("1");
+		objPolicyConversionParameters.setTargetConfiguration(CommonUtility.getStringFromInputStream(new FileInputStream("src/test/resource/fixtures/wsa/AllSettings.xml")));
+		DTDProvider.setAsyncosDTD(objPolicyConversionParameters.getTargetSoftware());
 		
-		ByteArrayOutputStream out = (ByteArrayOutputStream) policyConversionToolService.doWSAMigration(wsaMigrationParameters);
+		ByteArrayOutputStream out = (ByteArrayOutputStream) policyConversionToolService.doPolicyConversion(objPolicyConversionParameters);
 		
 		FileOutputStream outStream = new FileOutputStream("D:\\out.zip");
 		outStream.write(out.toByteArray());
 		out.close();
 		outStream.close();
+/*Need to close applicationcontext*/
 		 
 		logger.info("***********************************");
-		logger.info("Started Log Analysing Tool");
+		logger.info("Stopped Policy Conversion Tool");
 		logger.info("***********************************");
 		
 	}
