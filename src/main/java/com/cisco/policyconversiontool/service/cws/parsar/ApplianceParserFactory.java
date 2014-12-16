2931a28c485c63bed7a8ba9ac0e94bed8ae0fa11
kgadekar <kgadekar@securview.com>
2014-12-16 15:18:22 +0530

    no need to check source appliance

--- src/main/java/com/cisco/policyconversiontool/service/cws/parsar/ApplianceParserFactory.java
+++ src/main/java/com/cisco/policyconversiontool/service/cws/parsar/ApplianceParserFactory.java
@@ -1,13 +1,8 @@
 package com.cisco.policyconversiontool.service.cws.parsar;
 
-import com.cisco.policyconversiontool.service.util.Constants;
-
 public class ApplianceParserFactory {
-	public ApplianceParser getApplianceParser(String format) {
-		if(format.equals(Constants.APPLIENCE_ID_CWS))
-		{
+	public ApplianceParser getApplianceParser() {
+		 
 			return (ApplianceParser) new CWSParser();
-		}
-		return null;
 	}
 }
